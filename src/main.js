80367f55ddeaf0b814d9399e09f4b077d249fee2
Elan Trybuch <elan@trybuch.com>
2018-10-19 13:14:49 -0400

    rebase

--- src/main.js
+++ src/main.js
@@ -1,19 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import 'vuetify/dist/vuetify.min.css';
-
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 import App from './App';
 
-
 Vue.use(Vuetify);
 Vue.use(require('vue-moment'));
+
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
 });
